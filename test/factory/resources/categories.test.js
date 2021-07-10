8840df7b3de4e38849dc4641eec23da9b69e2a63
Ikenna Ojukwu <carteraxwebsolution@gmail.com>
2021-07-10 13:46:05 +0100

    :white_check_mark: more tests

--- test/factory/resources/categories.test.js
+++ test/factory/resources/categories.test.js
@@ -1,26 +1,34 @@
 const { expect } = require('chai');
-const { BN, expectRevert } = require('@openzeppelin/test-helpers');
-const { lorem } = require('faker');
+const { BN, expectEvent, expectRevert } = require('@openzeppelin/test-helpers');
 
-module.exports = function () {
-  it('deployer can create category', async function () {
-    await this.factory.createCategory('Technology', true);
-    await this.factory.createCategory('Health', true);
+module.exports = function() {
+  /* -------------------------------------------------------------------------- */
+  /*                               createCategory                               */
+  /* -------------------------------------------------------------------------- */
+  it('should create categories', async function() {
+    const receipt = await this.factory.createCategory('Technology', true);
 
+    expect((await this.factory.campaignCategories(0)).title).to.equal(
+      'Technology'
+    );
+    expect((await this.factory.campaignCategories(0)).active).to.equal(true);
+    expect((await this.factory.campaignCategories(0)).exists).to.equal(true);
     expect(await this.factory.categoryCount()).to.be.bignumber.equal(
-      new BN('2')
+      new BN('1')
     );
     expect(await this.factory.categoryIsTaken('Technology')).to.equal(true);
-    expect(await this.factory.categoryIsTaken('Health')).to.equal(true);
+    expectEvent(receipt, 'CategoryAdded', {
+      categoryId: new BN('0'),
+      title: 'Technology',
+      active: true,
+    });
   });
-
-  it('non managers cannot create category', async function () {
+  it('non managers cannot create category', async function() {
     await expectRevert.unspecified(
       this.factory.createCategory('Technology', true, { from: this.addr1 })
     );
   });
-
-  it('address with role can create category', async function () {
+  it('address with role can create category', async function() {
     const MANAGE_USERS = await this.factory.MANAGE_USERS();
     const MANAGE_CATEGORIES = await this.factory.MANAGE_CATEGORIES();
 
@@ -38,30 +46,46 @@ module.exports = function () {
     expect(await this.factory.categoryIsTaken('Technology')).to.equal(false);
     expect(await this.factory.categoryIsTaken('Sports')).to.equal(true);
   });
-
-  it('category name must be unique', async function () {
+  it('category creation should fail if category name is not unique', async function() {
     await this.factory.createCategory('Technology', true);
     await expectRevert.unspecified(
       this.factory.createCategory('Technology', true)
     );
   });
+  it('category creation should fail if contract is paused', async function() {
+    await this.factory.pauseCampaign();
+    await expectRevert.unspecified(
+      this.factory.createCategory('Technology', true)
+    );
+  });
+  it('category creation should work if contract is unpaused', async function() {
+    await this.factory.pauseCampaign();
+    await this.factory.unpauseCampaign();
+    await this.factory.createCategory('Charity', true);
+    expect(await this.factory.categoryIsTaken('Charity')).to.equal(true);
+  });
 
-  it('cannot delete a category that does not exist', async function () {
+  /* -------------------------------------------------------------------------- */
+  /*                               destroyCategory                              */
+  /* -------------------------------------------------------------------------- */
+  it('cannot delete a category that does not exist', async function() {
     expect(await this.factory.categoryIsTaken('Technology')).to.equal(false);
     await expectRevert.unspecified(this.factory.destroyCategory(0));
   });
-
-  it('manager with role can delete category', async function () {
+  it('manager with role can delete category', async function() {
     await this.factory.createCategory('Gaming', false);
-    await this.factory.destroyCategory(0);
-
+    const receipt = await this.factory.destroyCategory(0);
     const category = await this.factory.campaignCategories(0);
 
-    expect(category.exists).to.equal(false);
-    expect(await this.factory.categoryIsTaken(category.title)).to.equal(false);
+    expect(category.exists).to.be.equal(false);
+    expect(await this.factory.categoryIsTaken(category.title)).to.be.equal(
+      false
+    );
+    expectEvent(receipt, 'CategoryDestroyed', {
+      categoryId: new BN('0'),
+    });
   });
-
-  it('cannot delete categories without role', async function () {
+  it('cannot delete categories without role', async function() {
     await this.factory.createCategory('Gaming', false);
     await expectRevert.unspecified(
       this.factory.destroyCategory(0, { from: this.addr1 })
@@ -70,10 +94,28 @@ module.exports = function () {
     const category = await this.factory.campaignCategories(0);
 
     expect(category.exists).to.equal(true);
-    expect(await this.factory.categoryIsTaken(category.title)).to.equal(true);
+    expect(await this.factory.categoryIsTaken(category.title)).to.be.equal(
+      true
+    );
+  });
+  it('category destruction should fail if contract is paused', async function() {
+    await this.factory.createCategory('Charity', true);
+    await this.factory.pauseCampaign();
+    await expectRevert.unspecified(this.factory.destroyCategory(0));
+  });
+  it('category destruction should work if contract is unpaused', async function() {
+    await this.factory.createCategory('Charity', true);
+    await this.factory.pauseCampaign();
+    await this.factory.unpauseCampaign();
+    await this.factory.destroyCategory(0);
+    const category = await this.factory.campaignCategories(0);
+    expect(category.exists).to.be.equal(false);
   });
 
-  it('can return total amount of categories', async function () {
+  /* -------------------------------------------------------------------------- */
+  /*                                categoryCount                               */
+  /* -------------------------------------------------------------------------- */
+  it('can return categories count', async function() {
     await this.factory.createCategory('Cooking', true);
     await this.factory.createCategory('Health', false);
     await this.factory.createCategory('Technology', false);
@@ -83,34 +125,32 @@ module.exports = function () {
     );
   });
 
-  it('can return categories count', async function () {
-    await this.factory.createCategory('Cooking', true);
-    await this.factory.createCategory('Health', false);
-    await this.factory.createCategory('Technology', false);
-
-    expect(await this.factory.categoryCount()).to.be.bignumber.equal(
-      new BN('3')
-    );
-  });
-
-  it('manager with role can modify category', async function () {
+  /* -------------------------------------------------------------------------- */
+  /*                               modifyCategory                               */
+  /* -------------------------------------------------------------------------- */
+  it('manager with role can modify category', async function() {
     const MANAGE_CATEGORIES = await this.factory.MANAGE_CATEGORIES();
 
     await this.factory.createCategory('Sports', false);
     await this.factory.addRole(this.addr1, MANAGE_CATEGORIES);
 
-    await this.factory.modifyCategory(0, 'Sports', true, { from: this.addr1 });
-    expect((await this.factory.campaignCategories(0)).title).to.equal('Sports');
-    expect((await this.factory.campaignCategories(0)).active).to.equal(true);
-
-    await this.factory.modifyCategory(0, 'Health', false, { from: this.addr1 });
-    expect(await this.factory.categoryIsTaken('Health')).to.equal(true);
-    expect(await this.factory.categoryIsTaken('Sports')).to.equal(false);
+    const receipt = await this.factory.modifyCategory(0, 'Health', false, {
+      from: this.addr1,
+    });
+    expect(await this.factory.categoryIsTaken('Health')).to.be.equal(true);
+    expect(await this.factory.categoryIsTaken('Sports')).to.be.equal(false);
     expect((await this.factory.campaignCategories(0)).title).to.equal('Health');
-    expect((await this.factory.campaignCategories(0)).active).to.equal(false);
+    expect((await this.factory.campaignCategories(0)).active).to.be.equal(
+      false
+    );
+    expect((await this.factory.campaignCategories(0)).exists).to.be.equal(true);
+    expectEvent(receipt, 'CategoryModified', {
+      categoryId: new BN('0'),
+      title: 'Health',
+      active: false,
+    });
   });
-
-  it('user without role cannot modify category', async function () {
+  it('user without role cannot modify category', async function() {
     await this.factory.createCategory('Health', true);
 
     await expectRevert.unspecified(
@@ -119,15 +159,19 @@ module.exports = function () {
     expect((await this.factory.campaignCategories(0)).title).to.equal('Health');
     expect((await this.factory.campaignCategories(0)).active).to.equal(true);
   });
-
-  it("cannot create campaign if category isn't active and does not exist", async function () {
+  it('category modification should fail if contract is paused', async function() {
+    await this.factory.createCategory('Sports', true);
+    await this.factory.pauseCampaign();
     await expectRevert.unspecified(
-      this.factory.createCampaign(300, 0, lorem.sentence(), lorem.paragraph())
-    ); // cat does not exist
-
-    await this.factory.createCategory('Sports', false);
-    await expectRevert.unspecified(
-      this.factory.createCampaign(1200, 0, lorem.sentence(), lorem.paragraph())
-    ); // cat not active
+      this.factory.modifyCategory(0, 'Health', false)
+    );
+  });
+  it('category modification should work if contract is unpaused', async function() {
+    await this.factory.createCategory('Sports', true);
+    await this.factory.pauseCampaign();
+    await this.factory.unpauseCampaign();
+    await this.factory.modifyCategory(0, 'Health', true);
+    expect(await this.factory.categoryIsTaken('Health')).to.be.equal(true);
+    expect((await this.factory.campaignCategories(0)).exists).to.be.equal(true);
   });
 };
--- test/factory/resources/users.test.js
+++ test/factory/resources/users.test.js
@@ -1,8 +1,12 @@
 const { expect } = require('chai');
 const { internet } = require('faker');
 const { BN, expectEvent, expectRevert } = require('@openzeppelin/test-helpers');
+const { web3 } = require('@openzeppelin/test-helpers/src/setup');
 
 module.exports = function() {
+  /* -------------------------------------------------------------------------- */
+  /*                                   signUp                                   */
+  /* -------------------------------------------------------------------------- */
   it('user can signup', async function() {
     const email = internet.email();
     const username = internet.userName();
@@ -23,34 +27,67 @@ module.exports = function() {
     expect(user.exists).to.equal(true);
 
     expectEvent(receipt, 'UserAdded', {
-      id: userId,
+      userId: new BN(userId),
+      email: email,
+      username: username,
     });
   });
-
   it('user cannot sign up with taken username and email', async function() {
     await expectRevert.unspecified(
       this.factory.signUp(this.adminMail, this.adminUserName)
     );
   });
-
-  it('should return total amount of users', async function() {
-    const users = await this.factory.userCount();
-    expect(users).to.be.bignumber.equal(new BN('3'));
+  it('sign up should fail if factory is paused', async function() {
+    await this.factory.pauseCampaign();
+    await expectRevert.unspecified(
+      this.factory.signUp(internet.email(), internet.userName())
+    );
+  });
+  it('sign up should work if factory is unpaused', async function() {
+    await this.factory.pauseCampaign();
+    await this.factory.unpauseCampaign();
+    await this.factory.signUp(internet.email(), internet.userName());
+    const userId = await this.factory.userID(this.addr3);
+    const user = await this.factory.users(userId);
+    expect(user.exists).to.equal(true);
   });
 
+  /* -------------------------------------------------------------------------- */
+  /*                                  userCount                                 */
+  /* -------------------------------------------------------------------------- */
+  it('should return total amount of users', async function() {
+    const users = await this.factory.userCount();
+    expect(new BN(users)).to.be.bignumber.equal(new BN('3'));
+  });
+
+  /* -------------------------------------------------------------------------- */
+  /*                             toggleUserApproval                             */
+  /* -------------------------------------------------------------------------- */
   it('address with role can approve and disapprove user', async function() {
     const MANAGE_USERS = await this.factory.MANAGE_USERS();
     const userId = await this.factory.userID(this.addr2);
+    let receipt;
 
     await this.factory.addRole(this.addr1, MANAGE_USERS);
 
-    await this.factory.toggleUserApproval(userId, true, { from: this.addr1 });
+    receipt = await this.factory.toggleUserApproval(userId, true, {
+      from: this.addr1,
+    });
+    expectEvent(receipt, 'UserApproval', {
+      userId: new BN(userId),
+      approval: true,
+    });
     expect((await this.factory.users(userId)).verified).to.equal(true);
 
-    await this.factory.toggleUserApproval(userId, false, { from: this.addr1 });
+    receipt = await this.factory.toggleUserApproval(userId, false, {
+      from: this.addr1,
+    });
+    expectEvent(receipt, 'UserApproval', {
+      userId: new BN(userId),
+      approval: false,
+    });
     expect((await this.factory.users(userId)).verified).to.equal(false);
   });
-
   it('address without role cannot approve user', async function() {
     const userId = await this.factory.userID(this.addr2);
 
@@ -58,32 +95,52 @@ module.exports = function() {
       this.factory.toggleUserApproval(userId, true, { from: this.addr1 })
     );
   });
-
-  it('cannot modify user if they do not exist', async function() {
-    await expectRevert.unspecified(
-      this.factory.modifyUser(6, internet.email(), internet.userName())
-    );
+  it('user approval should fail if factory is paused', async function() {
+    await this.factory.pauseCampaign();
+    await expectRevert.unspecified(this.factory.toggleUserApproval(1, true));
+  });
+  it('user approval should work if factory is unpaused', async function() {
+    await this.factory.pauseCampaign();
+    await this.factory.unpauseCampaign();
+    await this.factory.toggleUserApproval(1, true);
+    expect((await this.factory.users(1)).verified).to.equal(true);
   });
 
+  /* -------------------------------------------------------------------------- */
+  /*                                 modifyUser                                 */
+  /* -------------------------------------------------------------------------- */
   it('user or manager can modify user details', async function() {
     const newUserName = internet.userName(),
       newEmail = internet.email();
 
-    await this.factory.modifyUser(0, newEmail, newUserName);
+    const receipt = await this.factory.modifyUser(0, newEmail, newUserName);
+    const block = await web3.eth.getBlock(receipt.receipt.blockNumber);
 
     const editedUser = await this.factory.users(0);
 
     expect(editedUser.username).to.equal(newUserName);
     expect(editedUser.email).to.equal(newEmail);
     expect(editedUser.verified).to.equal(false);
+    expect(new BN(editedUser.updatedAt)).to.be.bignumber.equal(
+      new BN(block.timestamp)
+    );
     expect(await this.factory.emailIsTaken(newEmail)).to.equal(true);
     expect(await this.factory.emailIsTaken(this.adminMail)).to.equal(false);
     expect(await this.factory.usernameIsTaken(newUserName)).to.equal(true);
     expect(await this.factory.usernameIsTaken(this.adminUserName)).to.equal(
       false
     );
+    expectEvent(receipt, 'UserModified', {
+      userId: new BN('0'),
+      email: newEmail,
+      username: newUserName,
+    });
   });
-
+  it('cannot modify user if they do not exist', async function() {
+    await expectRevert.unspecified(
+      this.factory.modifyUser(6, internet.email(), internet.userName())
+    );
+  });
   it("user cannot modify another user's profile", async function() {
     await expectRevert.unspecified(
       this.factory.modifyUser(0, internet.email(), internet.userName(), {
@@ -91,19 +148,42 @@ module.exports = function() {
       })
     );
   });
+  it('user modification should fail if factory is paused', async function() {
+    await this.factory.pauseCampaign();
+    await expectRevert.unspecified(
+      this.factory.modifyUser(0, internet.userName(), internet.email())
+    );
+  });
+  it('user modification should work if factory is unpaused', async function() {
+    let user, newUserName, newEmail, modifiedUser;
 
+    await this.factory.pauseCampaign();
+    await this.factory.unpauseCampaign();
+
+    user = await this.factory.users(1);
+    newUserName = internet.userName();
+    newEmail = internet.email();
+    await this.factory.modifyUser(1, newUserName, newEmail);
+    modifiedUser = await this.factory.users(1);
+    expect(new BN(user.updatedAt)).to.be.bignumber.lessThan(
+      new BN(modifiedUser.updatedAt)
+    );
+  });
+
+  /* -------------------------------------------------------------------------- */
+  /*                                 destroyUser                                */
+  /* -------------------------------------------------------------------------- */
   it('manager with role can delete user', async function() {
-    const user = await this.factory.users(2),
-      receipt = await this.factory.destroyUser(2);
+    const receipt = await this.factory.destroyUser(2),
+      user = await this.factory.users(2);
 
     expect((await this.factory.users(2)).exists).to.equal(false);
     expect((await this.factory.users(2)).verified).to.equal(false);
     expect(await this.factory.usernameIsTaken(user.username)).to.equal(false);
     expect(await this.factory.emailIsTaken(user.email)).to.equal(false);
 
-    expectEvent(receipt, 'UserRemoved', { id: new BN('2') });
+    expectEvent(receipt, 'UserRemoved', { userId: new BN('2') });
   });
-
   it('user without role cannot delete user', async function() {
     await expectRevert.unspecified(
       this.factory.destroyUser(0, { from: this.addr3 })
@@ -115,8 +195,15 @@ module.exports = function() {
     );
     expect(await this.factory.emailIsTaken(this.adminMail)).to.equal(true);
   });
-
-  it('should get users count', async function() {
-    expect(await this.factory.userCount()).to.be.bignumber.equal(new BN('3'));
+  it('user destruction should fail if factory is paused', async function() {
+    await this.factory.pauseCampaign();
+    await expectRevert.unspecified(this.factory.destroyUser(0));
+  });
+  it('user destruction should work if factory is unpaused', async function() {
+    await this.factory.pauseCampaign();
+    await this.factory.unpauseCampaign();
+    await this.factory.destroyUser(1);
+    const user = await this.factory.users(1);
+    expect(user.exists).to.be.equal(false);
   });
 };
